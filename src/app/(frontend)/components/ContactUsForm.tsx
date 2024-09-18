2223f8f9f33f3eca90da0d6b1d49e03c362f35bd
Gurwinder Singh <gurwinder.gwebs@gmail.com>
2024-09-18 16:58:18 +0530

    input type change

--- src/app/(frontend)/components/ContactUsForm.tsx
+++ src/app/(frontend)/components/ContactUsForm.tsx
@@ -77,7 +77,7 @@ export default function ContactUsForm() {
 
                                 <h4 className='text-[1.2rem] text-black dark:text-white mb-3'><strong>Step 3 -</strong> Add additional Details<span className="text-blue-500 dark:text-white dark:font-bold">(Required)</span></h4>
                                 <div className="relative mb-4">
-                                    <textarea type="email" id="emailAddress" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white border-b  border-gray-400 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " > </textarea>
+                                    <input type="email" id="emailAddress" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white border-b  border-gray-400 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " > </input>
                                     <label htmlFor="emailAddress" className="absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
                                         Enter Your Email Address
                                     </label>
