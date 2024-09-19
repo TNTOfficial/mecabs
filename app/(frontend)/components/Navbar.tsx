de45e5819416cd3754da3f577c7d5a5d27a2275d
Gurwinder Singh <gurwinder.gwebs@gmail.com>
2024-09-19 17:20:39 +0530

    about page updated

--- app/(frontend)/components/Navbar.tsx
+++ app/(frontend)/components/Navbar.tsx
@@ -105,6 +105,14 @@ export default function Navbar() {
                                     Home
                                 </Link>
                             </li>
+                            <li>
+                                <Link
+                                    href="/about"
+                                    className="block py-2 px-3 font-medium text-[1rem] uppercase text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                >
+                                    About
+                                </Link>
+                            </li>
                             
                         </ul>
                     </div>
