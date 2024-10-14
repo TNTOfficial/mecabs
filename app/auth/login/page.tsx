8506e6ec50a8278eb17f48d8963f429abe362be7
Ram Saini <sainiram226@gmail.com>
2024-10-14 17:57:47 +0530

    change pic form login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,20 @@
 import { LoginForm } from "@/features/auth/components/login-form";
 import React from "react";
 import { IoMdCheckmark } from "react-icons/io";
-import signcard from "@/public/signcard4.jpg";
+import signcard from "@/public/signcard5.jpg";
+import signbg from "@/public/signbg.jpg";
 
 const LoginPage = () => {
   return (
     <>
-      <section className="py-[50px] flex justify-center items-center min-h-screen">
+      <section style={{backgroundImage: `url(${signbg.src})`}}  className="py-[50px] flex justify-center items-center min-h-screen bg-center bg-cover bg-no-repeat relative z-0 before:h-full before:w-full before:bg-slate-900 before:bg-opacity-30 before:z-[-1] before:absolute before:top-0 before:start-0">
         <div className="flex m-auto flex-wrap justify-center items-stretch rounded-3xl lg:min-h-[730px] shadow-md border-t bg-white overflow-hidden lg:max-w-screen-lg  max-w-full ">
           <div className="lg:w-[calc(100%_/_2)] w-[calc(100%_/_1)] max-lg:hidden">
             <div
               className="card h-full relative z-0  bg-no-repeat bg-center bg-cover"
               style={{ backgroundImage: ` url(${signcard.src})` }}
             >
-              <div className="card_body bg-black bg-opacity-50  h-full w-full md:px-14 md:pb-14 p-10 md:pt-[70px] flex justify-start items-start flex-col">
+              <div className="card_body bg-black bg-opacity-50  h-full w-full md:px-14 md:pb-14 p-10 md:pt-[70px] flex justify-center items-start flex-col">
                 <h1 className="text-white text-[2rem] font-bold text-center">
                   Success starts here
                 </h1>
