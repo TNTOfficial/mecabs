8cd90e239482ab18de57839ebeff60a1e40ec30c
Tejinder Singh <tejinder94782@gmail.com>
2024-11-12 14:52:10 +0530

    sidebar toggler functionality added!

--- app/(dashboard)/dashboard/_components/sidebar.tsx
+++ app/(dashboard)/dashboard/_components/sidebar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import { Logo } from "./logo";
 import { SidebarRoutes } from "./sidebar-routes";
 
-export const Sidebar = () => {
+interface SidebarProps {
+  isOpen: boolean;
+}
+export const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
   return (
-    <aside className="flex flex-col shrink-0 w-[300px] top-0 fixed lg:left-0 left-[-300px] h-[100dvh] bg-white">
+    <aside
+      className={`flex flex-col shrink-0 w-[300px] top-0 fixed lg:left-0  h-[100dvh] bg-white ${
+        isOpen ? "left-0" : "left-[-300px]"
+      }`}
+    >
       <Logo />
       <SidebarRoutes />
     </aside>
