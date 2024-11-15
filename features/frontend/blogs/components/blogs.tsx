1c89df924da1848d03439d984312f73cf7f8f669
Ram Saini <sainiram226@gmail.com>
2024-11-15 18:00:29 +0530

    import bug solved

--- features/frontend/blogs/components/blogs.tsx
+++ features/frontend/blogs/components/blogs.tsx
@@ -13,7 +13,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { searchBlogs } from "@/actions/blog/search-blog";
 import { Blog } from "@/features/admin/blog/types";
-import banner6 from '@/public/banner6.webp'
+// import banner6 from '@/public/banner6.webp'
 
 interface BlogsProps {
   initialBlogs: Blog[];
