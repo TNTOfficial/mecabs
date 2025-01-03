0b162f59fc4003eb21666f32c1a7f56644a1fce0
Tejinder Singh <tejinder94782@gmail.com>
2025-01-03 16:20:41 +0530

    Removed unused pages.

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { requiredRole } from "@/lib/auth";
-import React from "react";
-
-const page = async () => {
-  await requiredRole("ADMIN");
-  return <div>I am test page</div>;
-};
-
-export default page;
